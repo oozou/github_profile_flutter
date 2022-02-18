ebfec9802885e29e2b402f15edc88faafefd0215
Wachirapong Prasertwong <ni@oozou.com>
2022-02-18 13:20:27 +0700

    use FutureBuilder for showing data from api

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -8,33 +8,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     HomeProvider homeProvider = Provider.of(context);
-    homeProvider.getGithubProfiles();
     return Scaffold(
       body: SafeArea(
         child: Container(
           color: Colors.grey.shade100,
-          child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: homeProvider.profiles.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 24.0,
-                          backgroundImage: NetworkImage(
-                              homeProvider.profiles[index].avatarUrl ?? ""),
-                          backgroundColor: Colors.transparent,
+          child: FutureBuilder(
+            future: homeProvider.getGithubProfiles(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: homeProvider.profiles.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 24.0,
+                              backgroundImage: NetworkImage(
+                                  homeProvider.profiles[index].avatarUrl ?? ""),
+                              backgroundColor: Colors.transparent,
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Text(homeProvider.profiles[index].login)
+                          ],
                         ),
-                        const SizedBox(width: 8,),
-                        Text(homeProvider.profiles[index].login)
-                      ],
-                    ),
-                  ),
-                );
-              }),
+                      ),
+                    );
+                  });
+            },
+          ),
         ),
       ),
     );
